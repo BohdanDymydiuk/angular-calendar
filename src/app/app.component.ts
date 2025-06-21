081648cba8e3f47b8f8c0ce3aa32a03c570cec82
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2025-06-21 14:38:36 +0300

    add calendar logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,25 @@ import { RouterOutlet } from '@angular/router';
 const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const weekLastIndex = week.length - 1;
 
-type MonthPosition = -1 | 0 | 1;
+enum MonthOffset {
+  prev = -1,
+  curr = 0,
+  next = 1,
+}
+
+enum MonthEnds {
+  first = 0,
+  last = 11,
+}
+
+interface Calendar {
+  [year: number]: {
+    [monthIndex: number]: {
+      name: string;
+      monthDays: number[];
+    }
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -17,104 +35,191 @@ type MonthPosition = -1 | 0 | 1;
 export class AppComponent {
   title = 'angular-calendar';
   week = week;
-
   date = new Date();
-  year = this.getYear();
+
+  monthIndex = this.date.getMonth();
+  prevMonthIndex = this.getPrevMonthIndex();
+  nextMonthIndex = this.getNextMonthIndex();
+
+  year = this.date.getFullYear();
   month = this.getMonth();
-  monthNum = this.getMonthNum();
 
-  currMonthLength = this.getMonthLength();
-  currMonthDays = this.getMonthDays();
+  monthLength = this.getMonthLength(MonthOffset.curr);
+  monthDays = this.getMonthDays(this.monthLength);
 
-  prevMonthLength = this.getMonthLength(-1);
-  prevMonthDays = this.getPrevMonthDays();
+  prevMonthLength = this.getMonthLength(MonthOffset.prev);
+  prevMonthDays = this.getPrevDays(this.getMonthDays(this.prevMonthLength));
 
-  nextMonthLength = this.getMonthLength(1);
-  nextMonthDays = this.getNextMonthDays();
+  nextMonthLength = this.getMonthLength(MonthOffset.next);
+  nextMonthDays = this.getNextDays(this.getMonthDays(this.nextMonthLength));
 
+  calendar: Calendar = {
+    [this.year]: {
+      [this.monthIndex]: {
+        name: this.month,
+        monthDays: this.monthDays,
+      }
+    }
+  };
 
-  // #region get
-  // #region rest
+  getMonth(year = this.year, monthIndex = this.monthIndex) {
+    const date = new Date(year, monthIndex);
 
-  getYear() {
-    return this.date.getFullYear();
+    return date.toLocaleString('default', { month: 'long' });
   }
 
-  getMonth() {
-    return this.date.toLocaleString('default', { month: 'long' });
+  getPrevMonthIndex() {
+    return (this.monthIndex + MonthOffset.prev) < MonthEnds.first ? MonthEnds.last : this.monthIndex + MonthOffset.prev;
   }
 
-  getMonthNum() {
-    return this.date.getMonth();
+  getNextMonthIndex() {
+    return (this.monthIndex + MonthOffset.next) > MonthEnds.last ? MonthEnds.first : this.monthIndex + MonthOffset.next;
   }
 
-  getMonthDays(monthLength = this.currMonthLength) {
+  getMonthLength(monthOffset: MonthOffset) {
+    const { monthIndex, year, calendar } = this;
+    const month = monthIndex + monthOffset;
+    const nextYear = year + 1;
+    const prevYear = year - 1;
+
+    if (
+      monthIndex === MonthEnds.first &&
+      !calendar[prevYear] &&
+      monthOffset === MonthOffset.prev
+    ) {
+      this.setPrevYear(prevYear);
+    }
+
+    if (
+      monthIndex === MonthEnds.last &&
+      !calendar[nextYear] &&
+      monthOffset === MonthOffset.next
+    ) {
+      console.log(this.month);
+
+
+      this.setNextYear(nextYear);
+    }
+
+    return new Date(this.year, month + 1, 0).getDate();
+  }
+
+  getMonthDays(monthLength: number) {
     return Array.from({ length: monthLength }, (_, i) => i + 1);
   }
 
-  getMonthLength(monthPosition: MonthPosition = 0) {
-    const { year, monthNum } = this;
-    const month = monthNum + monthPosition + 1;
-
-    return new Date(year, month, 0).getDate();
+  getPrevDays(days: number[]) {
+    return days.slice(this.prevMonthLength - this.firstWeekDay);
   }
 
-  getPrevMonthDays() {
-    return this.getMonthDays(this.prevMonthLength).slice(this.prevMonthLength - this.firstWeekDay);
+  getNextDays(days: number[]) {
+    return days.slice(0, weekLastIndex - this.lastWeekDay);
   }
 
-  getNextMonthDays() {
-    return this.getMonthDays(this.nextMonthLength).slice(0, weekLastIndex - this.lastWeekDay);
+  setPrevYear(prevYear: number) {
+    if (!this.calendar[prevYear]) {
+      this.calendar[prevYear] = {};
+    }
+
+    const name = this.getMonth(prevYear, MonthEnds.last);
+    const monthDays = this.getMonthDays(this.nextMonthLength);
+
+    this.calendar[prevYear][MonthEnds.last] = { name, monthDays };
   }
 
-  // #endregion
+  setNextYear(nextYear: number) {
+    if (!this.calendar[nextYear]) {
+      this.calendar[nextYear] = {};
+    }
+
+    const name = this.getMonth(nextYear, MonthEnds.first);
+    const monthDays = this.getMonthDays(this.nextMonthLength);
+
+    this.calendar[nextYear][MonthEnds.first] = { name, monthDays };
+  }
 
   get firstWeekDay() {
-    const { year, monthNum } = this;
-    const date = new Date(year, monthNum, 1);
+    const { year, monthIndex } = this;
+    const date = new Date(year, monthIndex, 1);
 
     return date.getDay();
   }
 
   get lastWeekDay() {
-    const { year, monthNum, currMonthLength } = this;
-    const date = new Date(year, monthNum, currMonthLength);
+    const { year, monthIndex, monthLength } = this;
+    const date = new Date(year, monthIndex, monthLength);
 
     return date.getDay();
   }
 
-  // #endregion
-
   updateDate() {
-    this.month = this.getMonth();
-    this.monthNum = this.getMonthNum();
+    this.prevMonthIndex =
+      (this.monthIndex + MonthOffset.prev) < MonthEnds.first ?
+        MonthEnds.last :
+        this.monthIndex + MonthOffset.prev;
 
+    this.nextMonthIndex =
+      (this.monthIndex + MonthOffset.next) > MonthEnds.last ?
+        MonthEnds.first :
+        this.monthIndex + MonthOffset.next;
 
-    this.currMonthLength = this.getMonthLength();
-    this.currMonthDays = this.getMonthDays();
+    this.monthLength = this.getMonthLength(MonthOffset.curr);
+    this.monthDays = this.getMonthDays(this.monthLength);
 
-    this.prevMonthLength = this.getMonthLength(-1);
-    this.prevMonthDays = this.getPrevMonthDays();
+    this.prevMonthLength = this.getMonthLength(MonthOffset.prev);
+    this.prevMonthDays = this.getPrevDays(this.getMonthDays(this.prevMonthLength));
 
-    this.nextMonthLength = this.getMonthLength(1);
-    this.nextMonthDays = this.getNextMonthDays();
-
-    if (this.year !== this.getYear()) {
-      this.year = this.getYear()
-    }
-  }
-
-  // #region hadnlers
-
-  nextMonthHandler() {
-    this.date.setMonth(this.date.getMonth() + 1);
-    this.updateDate();
+    this.nextMonthLength = this.getMonthLength(MonthOffset.next);
+    this.nextMonthDays = this.getNextDays(this.getMonthDays(this.nextMonthLength));
   }
 
   prevMonthHandler() {
-    this.date.setMonth(this.date.getMonth() - 1);
-    this.updateDate();
+    const { monthIndex, prevMonthIndex, prevMonthLength, calendar, year } = this;
+    const prevYear = year - 1;
+
+    this.monthIndex = prevMonthIndex;
+
+
+    if (monthIndex === MonthEnds.first) {
+      this.year = prevYear;
+
+      this.month = calendar[prevYear][MonthEnds.last].name;
+    } else {
+      if (!calendar[year][prevMonthIndex]) {
+        const name = this.getMonth(year, prevMonthIndex);
+        const monthDays = this.getMonthDays(prevMonthLength);
+
+        this.calendar[year][prevMonthIndex] = { name, monthDays };
+      }
+
+      this.month = calendar[year][prevMonthIndex].name;
+    }
+
+    this.updateDate()
   }
 
-  // #endregion
+  nextMonthHandler() {
+    const { monthIndex, nextMonthIndex, nextMonthLength, calendar, year } = this;
+    const nextYear = year + 1;
+
+    this.monthIndex = nextMonthIndex;
+
+
+    if (monthIndex === MonthEnds.last) {
+      this.year = nextYear;
+
+      this.month = calendar[nextYear][MonthEnds.first].name;
+    } else {
+      if (!calendar[year][nextMonthIndex]) {
+        const name = this.getMonth(year, nextMonthIndex);
+        const monthDays = this.getMonthDays(nextMonthLength);
+
+        this.calendar[year][nextMonthIndex] = { name, monthDays };
+      }
+
+      this.month = calendar[year][nextMonthIndex].name;
+    }
+
+    this.updateDate()
+  }
 }
